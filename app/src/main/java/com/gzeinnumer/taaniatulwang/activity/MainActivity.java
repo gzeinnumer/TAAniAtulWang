d2e49a0660bf4e1bc382ca5490a83c79087b9bf2
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2019-08-14 06:52:32 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/taaniatulwang/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.gzeinnumer.taaniatulwang.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.gzeinnumer.taaniatulwang.R;
+
+public class  MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
